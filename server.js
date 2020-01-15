dd4a97bbae6073e949bc7df3848fdba456b843d2
Dilyen <davidandohacquah@gmail.com>
2020-01-15 11:49:45 +0000

    commented

--- server.js
+++ server.js
@@ -2,84 +2,84 @@
 const express = require('express');
 const path = require('path');
 
-const SamlStrategy = require("passport-saml").SamlStrategy
-const passport = require("passport");
-const bodyParser = require("body-parser");
-const cookiesSession = require("cookies-session");
-const cookiesParser = require("cookies-parser");
+// const SamlStrategy = require("passport-saml").SamlStrategy
+// const passport = require("passport");
+// const bodyParser = require("body-parser");
+// const cookiesSession = require("cookies-session");
+// const cookiesParser = require("cookies-parser");
 
-let userEmail = "";
+// let userEmail = "";
 const app = express();
 
 
-app.use(cookieParser());
-app.use(
-  cookieSession({
-    name: "dummySession",
-    keys: ["super secret"],
-    maxAge: 2 * 24 * 60 * 60 * 1000 // 2 days
-  })
-);
+// app.use(cookieParser());
+// app.use(
+//   cookieSession({
+//     name: "dummySession",
+//     keys: ["super secret"],
+//     maxAge: 2 * 24 * 60 * 60 * 1000 // 2 days
+//   })
+// );
 
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(bodyParser.json());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.use(
-    new SamlStrategy(
-      {
-        protocol: "https://",
-        entryPoint: process.env.ENTRY_POINT, // SSO URL (Step 2)
-        issuer: process.env.ISSUER, // Entity ID (Step 4)
-        path: "/auth/saml/callback", // ACS URL path (Step 4)
-        cert: process.env.CERT
-      },
-      function (profile, done) {
-        // Parse user profile data
-        userEmail = profile.nameID;
-        return done(null, {
-          email: profile.email,
-          name: profile.name
-        });
-      }
-    )
-  );
-  passport.serializeUser(function (user, done) {
-    done(null, user);
-  });
-  passport.deserializeUser(function (user, done) {
-    done(null, user);
-  });
+// passport.use(
+//     new SamlStrategy(
+//       {
+//         protocol: "https://",
+//         entryPoint: process.env.ENTRY_POINT, // SSO URL (Step 2)
+//         issuer: process.env.ISSUER, // Entity ID (Step 4)
+//         path: "/auth/saml/callback", // ACS URL path (Step 4)
+//         cert: process.env.CERT
+//       },
+//       function (profile, done) {
+//         // Parse user profile data
+//         userEmail = profile.nameID;
+//         return done(null, {
+//           email: profile.email,
+//           name: profile.name
+//         });
+//       }
+//     )
+//   );
+//   passport.serializeUser(function (user, done) {
+//     done(null, user);
+//   });
+//   passport.deserializeUser(function (user, done) {
+//     done(null, user);
+//   });
 
-  app.get(
-    "/login",
-    passport.authenticate("saml", {
-      successRedirect: "/",
-      failureRedirect: "/login"
-    })
-  );
+//   app.get(
+//     "/login",
+//     passport.authenticate("saml", {
+//       successRedirect: "/",
+//       failureRedirect: "/login"
+//     })
+//   );
   
-app.post(
-    "/auth/saml/callback",
-    bodyParser.urlencoded({ extended: false }),
-    passport.authenticate("saml", {
-      failureRedirect: "/error",
-      failureFlash: false
-    }),
-    function (req, res) {
-      // sets a cookie called ttemail and sets its max age to 1 day
-      res.cookie('ttemail', userEmail, { maxAge: 1 * 24 * 60 * 60 * 1000, secure: true, httpOnly: false })
-      res.redirect("https://turntabl-pollster.herokuapp.com/new-poll");
-    }
-  );
+// app.post(
+//     "/auth/saml/callback",
+//     bodyParser.urlencoded({ extended: false }),
+//     passport.authenticate("saml", {
+//       failureRedirect: "/error",
+//       failureFlash: false
+//     }),
+//     function (req, res) {
+//       // sets a cookie called ttemail and sets its max age to 1 day
+//       res.cookie('ttemail', userEmail, { maxAge: 1 * 24 * 60 * 60 * 1000, secure: true, httpOnly: false })
+//       res.redirect("https://turntabl-pollster.herokuapp.com/new-poll");
+//     }
+//   );
 
-  app.all("*", function (req, res, next) {
-    if (req.isAuthenticated() || process.env.NODE_ENV !== "production") {
-      next();
-    } else {
-      res.redirect("/login");
-    }
-  });
+//   app.all("*", function (req, res, next) {
+//     if (req.isAuthenticated() || process.env.NODE_ENV !== "production") {
+//       next();
+//     } else {
+//       res.redirect("/login");
+//     }
+//   });
   // Serve only the static files form the dist directory
   app.use(express.static(__dirname + '/dist/poll-ui'));
   
